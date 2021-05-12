67e27d5dfcd4fbd95b9a80297305e98e5a811c2f
BOSSmax1 <83688996+BOSSmax1@users.noreply.github.com>
2021-05-12 15:02:17 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -23,13 +23,13 @@ async def start(bot, update):
         if (file_id or file_type) == None:
             return
         
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>" + 𝚃𝚑𝚊𝚗𝚔𝚢𝚘𝚞 𝙵𝚘𝚛 𝚄𝚜𝚒𝚗𝚐 𝙾𝚞𝚛 𝚂𝚎𝚛𝚟𝚒𝚌𝚎 And Don't forget to share our Group/Channel links to your friends 😜
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>" + 𝚃𝚑𝚊𝚗𝚔𝚢𝚘𝚞 𝙵𝚘𝚛 𝚄𝚜𝚒𝚗𝚐 𝙾𝚞𝚛 𝚂𝚎𝚛𝚟𝚒𝚌𝚎 And Don't forget to share our Group/Channel links to your friends 😜 \n
  
- \n 😎𝕁𝕠𝕚𝕟 𝕆𝕦𝕣  Group😎
+  😎𝕁𝕠𝕚𝕟 𝕆𝕦𝕣  Group😎 \n
  
- \n ⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱ 
+  ⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱ \n
  
- \n 𝕲𝖗𝖔𝖚𝖕 : @cinemahub_boss)
+  𝕲𝖗𝖔𝖚𝖕 : @cinemahub_boss)\n
         
         if file_type == "document":
         
